cf9af77cd52632077a8ea361f607e99816ab8702
alribeiro753 <alribeiro@id.uff.br>
2026-04-09 18:16:22 -0300

    Script com medidas

---
+++ Script com medidas.R
@@ -0,0 +1,14 @@
+
+# Script com medidas da variável 'imposto_anual'
+
+media_impostoanual = mean(imposto_anual)
+media_impostoanual
+# Média = 788.425
+
+mediana_impostoanual = median(imposto_anual)
+mediana_impostoanual
+# Mediana = 784
+
+desviopadrao_impostoanual = sd(imposto_anual)
+desviopadrao_impostoanual
+# Desvio Padrão = 211.3394
